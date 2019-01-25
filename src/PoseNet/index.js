5e104245aca96377342ed7d27bfb05a2ff62ba8a
Phelps <phelpsmemo@gmail.com>
2019-01-24 22:25:34 -0200

    Implementação da classe de predição com PoseNet

---
+++ src/PoseNet/index.js
@@ -0,0 +1,67 @@
+import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+
+import { EventEmitter } from 'events';
+
+
+/**
+ * Rede neural para a identificação dos pontos faciais do usuário
+ */
+class PoseNet extends EventEmitter {
+
+    constructor(video, imageMultiplier=0.75, imageScaleFactor=0.5, outputStride=16) {
+        this.isOn = true;
+
+        this.video = video;
+        
+        this.neuralModel = {};
+        this.imageMultiplier = imageMultiplier;
+        this.imageScaleFactor = imageScaleFactor;
+        this.outputStride = outputStride;
+    }
+
+    /**
+     * Método para criar um singleton da instância do modelo da rede
+     */
+    async buildNet() {
+        if (this.neuralModel) {
+            this.neuralModel = await posenet.load(this.imageMultiplier);
+        } 
+        return this.neuralModel;
+    }
+
+    /**
+     * Método para recuperar pontos faciais do usuário
+     */
+    async trackSingleUser() {
+        buildNet();
+
+        let poses = await this.neuralModel.estimateSinglePose(
+                    this.video, this.imageScaleFactor, false, this.outputStride)
+        
+        // Enviando evento que pode ser capturado por utilizadores da classe
+        this.emit("poses", poses);
+        
+        if (this.video && this.isOn) {
+            return tf.nextFrame().then(() => this.trackSingleUser());
+        }
+    }
+
+    /**
+     * Método para paralizar as predições 
+     */
+    stop() {
+        this.isOn = false;
+    }
+
+    /**
+     * Método para continuar as predições
+     */
+    start() {
+        this.isOn = true;
+    }
+}
+
+export {
+    PoseNet
+}
